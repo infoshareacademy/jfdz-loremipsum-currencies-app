cbc624bbb4a7b5730531247a6597e0a8142b2494
Marcin Bogucki <mar.bogucki@gmail.com>
2016-10-09 22:28:18 +0200

    sign in button

--- src/signin/Signin.js
+++ src/signin/Signin.js
@@ -18,10 +18,11 @@ export default class Signin extends Component {
 
     componentDidMount() {
         this.gapi.signin2.render('g-signin2', {
-            'scope': 'https://www.googleapis.com/auth/plus.login',
-            'width': 200,
-            'height': 200,
+            'scope': 'profile email',
+            'width': 340,
+            'height': 80,
             'longtitle': true,
+            'theme': 'dark',
             'onsuccess': this.onSuccess,
             'onfailure': this.onFailure
         })
@@ -31,11 +32,10 @@ export default class Signin extends Component {
         return (
           <div className="singin-container">
               <Jumbotron className="signin-jumbotron">
-                  <h1
+                  <h1>
                       Currency Exchange
                   </h1>
                   <p>Sign In with your social account</p>
-                  aaa - {this.cos}
                   <p>
                       <div className="g-signin2"></div>
                   </p>
