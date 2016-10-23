8f211326335422a490e6464449b1618ef930441f
natalia_s <slugocka.n@gmail.com>
2016-10-23 11:56:53 +0200

    changes to index.js

---
+++ src/store.js
@@ -0,0 +1,16 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+
+const reducer = combineReducers({
+
+})
+
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(
+        thunkMiddleware
+    )
+)
+
+export default store
