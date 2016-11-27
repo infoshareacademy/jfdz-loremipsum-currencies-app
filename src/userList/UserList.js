eed2ec50ed2f5eda2b16e3e718b41f499cae1b2f
tomaszlubkowski <tomlubkowski@gmail.com>
2016-11-27 07:11:06 +0100

    Showing User List from localstorage

--- src/userList/UserList.js
+++ src/userList/UserList.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import Heading from '../layout/Heading';
 
+
+
+function loadUserList(){
+    var userList='';
+    for (var name in localStorage)
+        userList += (name);
+    return (<tr><td>{userList}</td></tr>)
+}
+
 export default class UserList extends Component {
     render() {
         return (
             <div>
                 <Heading>User List</Heading>
+                <table>
+                    {loadUserList()}
+                </table>
             </div>
         );
     }
